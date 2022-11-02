1b84ad7f5b66285cb2ebcc9c7dcf3418f92c21f4
lahasya <lahasya.lnu001@umb.edu>
2022-11-02 09:39:30 -0400

    changes in folder structure

--- HW3/test/src/edu/umb/cs680/hw03/SingletonDemoTest.java
+++ HW3/test/src/edu/umb/cs680/hw03/SingletonDemoTest.java
@@ -1,11 +1,10 @@
-
+package edu.umb.cs680.hw03;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
-import umb.edu.cs680.hw03.SingletonDemo;
 
 public class SingletonDemoTest {
     @Test
